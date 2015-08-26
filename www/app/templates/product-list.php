264151c79513531e0eb7bdf571988e9f06cd83e1
Ben Abbott <jaminscript@gmail.com>
2015-08-26 15:18:16 +1200

    Display basic list of products

--- www/app/templates/product-list.php
+++ www/app/templates/product-list.php
@@ -1,2 +1,23 @@
 <h1>All Products</h1>
 
+<?php
+	
+	// SQL to get all products
+	$sql = "SELECT id, name, price, quantity FROM products";
+
+	// Run the SQL
+	$result = $dbc->query($sql);
+
+	// Loop through the result
+	while($row = $result->fetch_assoc()) {
+
+		echo '<h2>'.$row['name'].'</h2>';
+
+		echo '<ul>';
+		echo '<li>Price: $'.$row['price'].'</li>';
+		echo '<li>Quantity: '.$row['quantity'].'</li>';
+		echo '</ul>';
+
+	}
+
+?>
