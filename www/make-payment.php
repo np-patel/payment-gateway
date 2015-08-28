d9bcd48e5a459e21012f62a6cd549d84c8c1f564
Nehal Patel <nepatel30@gmail.com>
2015-08-28 15:18:26 +1200

    added product order and payment system

--- www/make-payment.php
+++ www/make-payment.php
@@ -1,6 +1,49 @@
 <?php
 
+// Start the session
+session_start();
+
+//get the config file
+require '../config.php';
+
 require 'app/PxPay_Curl.inc.php';
 
-//create an instance of the file
-$pxpay = new PxPay_Curl();
+//create an instance of the library
+$pxpay = new PxPay_Curl('https://sec.paymentexpress.com/pxpay/pxaccess.aspx', $PxPay_Userid, $PxPay_Key);
+
+//Create a new request object
+$request = new PxPayRequest();
+
+//prepare the url to come back to once payment has been complete
+$http_host = getenv('HTTP_HOST'); //localhost. if online than domain name
+
+$folders = getenv('SCRIPT_NAME');
+$urlToComeBackTo = 'http://'.$http_host.$folders;
+
+//loopthrough cart and calculate the grand total
+$grandTotal = 0;
+foreach ($_SESSION['cart'] as $cartItem) {
+	$grandTotal += $cartItem['quantity'] * $cartItem['price'];
+}
+
+//prepare data for PxPay 
+$request->setAmountInput($grandTotal);
+$request->setTxnType('Purchase'); //transaction type
+$request->setCurrencyInput('NZD');
+$request->setUrlFail( PROJECT_ROOT.'payment-response.php');
+$request->setUrlSuccess( PROJECT_ROOT.'payment-response.php');
+$request->setTxnData1('Nehal Patel');
+$request->setTxnData2('20 Kent Terrace, Wellington, NZ');
+$request->setTxnData3('Somthing else');
+
+//Prepare the request string out of the request data
+$requestString = $pxpay->makeRequest($request);
+
+//send the request to be processed
+$response = new MifMessage($requestString);
+
+//extract the url so we can redirect the user
+$urlToGoTo = $response->get_element_text('URI');
+
+//redirect the user
+header('Location: '.$urlToGoTo);
---
+++ www/payment-response.php
@@ -0,0 +1,69 @@
+<?php
+
+// Start the session
+session_start();
+
+//get the config file
+require '../config.php';
+
+//obtain the result from the address bar
+$result = $_GET['result'];
+
+require 'app/PxPay_Curl.inc.php';
+
+//create an instance of the library
+$pxpay = new PxPay_Curl('https://sec.paymentexpress.com/pxpay/pxaccess.aspx', $PxPay_Userid, $PxPay_Key);
+
+//extract the result
+//get the respones from the payment express
+$response = $pxpay->getResponse($result);
+
+echo '<pre>';
+print_r($response);
+
+$dbc = new mysqli('localhost', 'root', '', 'payment_gatway');
+
+//If the transaction was a success
+if($response->getSuccess() ){
+	//success :)
+	$customerName = $response->getTxnData1();
+	$customerAddress = $response->getTxnData2();
+
+	//mix the data
+	$contact = $customerName."\n\n".$customerAddress;
+
+	//filter the contect info
+	$contact = $dbc->real_escape_string($contact);
+
+	//create a new order
+	$sql = "INSERT INTO orders VALUES(NULL, 'Approved', 'panding', '$contact', CURRENT_TIMESTAMP, NULL)";
+
+	//run the sql
+	$dbc->query($sql);
+
+	//get the id of the new order
+	$orderID = $dbc->insert_id;
+
+
+
+	//loop through the cart contect
+	foreach ($_SESSION['cart'] as $cartItem) {
+		
+		$productID = $cartItem['id'];
+		$quantity = $cartItem['quantity'];
+		$price = $cartItem['price'];
+
+		//prepare the sql
+
+		 $sql = "INSERT INTO ordered_products VALUES(NULL, $orderID, $productID, $quantity, $price)";
+
+		
+
+		//run the sql
+		$dbc->query($sql);
+	}
+
+
+} else{
+	//fail :(
+}
